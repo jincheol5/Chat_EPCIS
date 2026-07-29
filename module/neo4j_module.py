e7ed1576ebd80d6de185da21b1cbe1dfad1b4e79
jincheol <oscar4293@gmail.com>
2026-07-29 20:16:54 +0900

    update

--- module/neo4j_module.py
+++ module/neo4j_module.py
@@ -1,5 +1,34 @@
+import networkx as nx
 from neo4j import GraphDatabase
+from neo4j.exceptions import Neo4jError
 
 class Neo4j_Interface:
-    def __init__(self):
-        pass
+    def __init__(self,port:int=7687):
+        try:
+            self.driver=GraphDatabase.driver(
+                f"neo4j://127.0.0.1:{port}"
+            )
+            self.driver.verify_connectivity()
+            print("Neo4j 연결 성공")
+        except Neo4jError as e:
+            print(f"Neo4j error: {e}")
+
+    def connect_db(self,port:int=7687):
+        try:
+            self.driver=GraphDatabase.driver(
+                f"neo4j://127.0.0.1:{port}"
+            )
+            self.driver.verify_connectivity()
+            print("Neo4j 연결 성공")
+        except Neo4jError as e:
+            print(f"Neo4j error: {e}")
+
+    def disconnect_db(self):
+        if self.driver is not None:
+            self.driver.close()
+            self.driver=None
+            print("Neo4j database disconnected!")
+
+    def insert_graph(self,graph:nx.MultiDiGraph):
+        """
+        """
